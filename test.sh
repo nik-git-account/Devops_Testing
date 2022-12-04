6138de1efb55daa9be72244b45ea6eec5581b214
NIkita <malinikita955@gmail.com>
2022-12-04 13:34:05 +0530

    feature files

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Author : Zara Ali
+# Copyright (c) Tutorialspoint.com
+# Script follows here:
+pwd
+ls
+
+echo "Welcome in Devops"
+read BATCH_NO
+echo "Devops Batch, $BATCH_NO"
+
+NAME="Nikita"
+echo $NAME
+readonly NAME
+NAME="Nik"
